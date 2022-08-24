8eef0fdb4857403456c9e59030cdb525b763f980
Rebecca Ross <b.laurenross@gmail.com>
2022-08-24 18:10:34 +0100

    modified intro to React ex

--- part_1/courseinfo/src/App.js
+++ part_1/courseinfo/src/App.js
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-        <Part part1={props.part1} exercises1={props.exercises1}/>
-        <Part part2={props.part2} exercises2={props.exercises2}/>
-        <Part part3={props.part3} exercises3={props.exercises3}/> 
+        <Part part={props.part1} exercises={props.exercises1}/>
+        <Part part={props.part2} exercises={props.exercises2}/>
+        <Part part={props.part3} exercises={props.exercises3}/>
     </>
   )
 }
@@ -19,9 +19,7 @@ const Content = (props) => {
 const Part = (props) => {
   return (
     <>
-        <p>{props.part1} {props.exercises1}</p>
-        <p>{props.part2} {props.exercises2}</p>
-        <p>{props.part3} {props.exercises3}</p>
+        <p>{props.part} {props.exercises}</p>
     </>
   )
 }
