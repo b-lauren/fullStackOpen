d1d1ac6310d1f13af19df895795cfb2c23ffdb0c
Rebecca Ross <b.laurenross@gmail.com>
2022-08-24 17:25:28 +0100

    exercise 1.2 refactor

--- part_1/courseinfo/src/App.js
+++ part_1/courseinfo/src/App.js
@@ -1,6 +1,38 @@
-import { Header } from './Header'
-import { Content } from './Content'
-import { Total } from './Total'
+const Header = (props) => {
+  return (
+    <>
+       <h1>{props.course}</h1> 
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+        <Part part1={props.part1} exercises1={props.exercises1}/>
+        <Part part2={props.part2} exercises2={props.exercises2}/>
+        <Part part3={props.part3} exercises3={props.exercises3}/> 
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+        <p>{props.part1} {props.exercises1}</p>
+        <p>{props.part2} {props.exercises2}</p>
+        <p>{props.part3} {props.exercises3}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+        <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
 
 
 const App = () => {
