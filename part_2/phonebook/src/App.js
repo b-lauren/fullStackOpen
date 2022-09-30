6430732653123d52b6ffc432d8c57d18e87d3f99
Rebecca Ross <b.laurenross@gmail.com>
2022-09-30 12:35:34 +0100

    exercise 2.6

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,24 +1,45 @@
 import { useState } from 'react'
+import { Person } from './components/Person'
 
 const App = () => {
+  //initialises the piece of state stored in persons 
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('add someone')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newNameObj = {
+      name: newName,
+      id: Math.random(),
+    }
+    setPersons(persons.concat(newNameObj))
+    setNewName('')
+  }
+
+  //sychronises the changes made with the components state
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => 
+          <Person person={person} key={person.name}/>
+          )}
+      </ul>
     </div>
   )
 }
---
+++ part_2/phonebook/src/components/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Person = ({person}) => {
+  return (
+    <li>{person.name}</li>
+  )
+}
