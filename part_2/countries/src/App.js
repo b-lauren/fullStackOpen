275b6ef2042c4a8bf8272bfa4b2651b50b5ef895
Rebecca Ross <b.laurenross@gmail.com>
2022-10-09 11:55:54 +0100

    set up project and API call

--- part_2/countries/src/App.js
+++ part_2/countries/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then((response) => {
+      console.log(response.data);
+      setCountries(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello</p>
     </div>
   );
 }
