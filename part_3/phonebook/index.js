85cab0d238a079206cd869ad432d8abeeeb7526a
Rebecca Ross <b.laurenross@gmail.com>
2022-11-11 11:52:36 +0000

    ex 3.2

--- part_3/phonebook/index.js
+++ part_3/phonebook/index.js
@@ -32,6 +32,16 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response, Persons) => {
+  const numberOfPeople = Persons.length;
+  const todaysDate = new Date();
+
+  response.send(`<div>Phonebook has info for ${numberOfPeople} people</div>
+    <div>${todaysDate}</div>
+  `);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
+
