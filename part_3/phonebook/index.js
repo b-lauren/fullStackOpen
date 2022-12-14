e92129601cd4f7e4de08b1d28465c69994ac1fbf
Rebecca Ross <b.laurenross@gmail.com>
2022-12-14 17:11:03 +0000

    added Morgan middleware

--- part_3/phonebook/index.js
+++ part_3/phonebook/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
-app.use(express.json())
-var morgan = require('morgan')
+var morgan = require('morgan');
+
+app.use(express.json());
+app.use(morgan('tiny'));
+
+morgan.token('body', function (req) {
+  return JSON.stringify(req.body);
+});
 
 let persons = [
   {
@@ -43,58 +49,55 @@ app.get('/info', (request, response, Persons) => {
   `);
 });
 
-//Fetching a single resource by ID 
+//Fetching a single resource by ID
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const currentPerson = persons.find(person => person.id === id)
-  
-  if (currentPerson) {
-    response.json(currentPerson)
-  } else {
-    response.status(404).end()
-  }
-})
+  const id = Number(request.params.id);
+  const currentPerson = persons.find((person) => person.id === id);
 
-//Delete a single entry with a HTTP delete 
+  if (currentPerson) {
+    response.json(currentPerson);
+  } else {
+    response.status(404).end();
+  }
+});
+
+//Delete a single entry with a HTTP delete
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const currentPerson = persons.filter(person => person.id !== id)
+  const id = Number(request.params.id);
+  const currentPerson = persons.filter((person) => person.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
 //Add a resource
 
-app.post('/api/persons', (request, response) => { 
-  const body = request.body  
-  const randId = Math.random()*100
-  const person = request.body 
-  person.id = randId 
-  console.log(person) 
-  
+app.post('/api/persons', morgan(':body'), (request, response) => {
+  const body = request.body;
+  const randId = Math.random() * 100;
+  const person = request.body;
+  person.id = randId;
+  console.log(person);
+
   if (body.name === undefined || body.number === undefined) {
     return response.status(400).json({
-      error: "name missing"
+      error: 'name missing',
     });
   }
 
-  const existingPerson = persons.find(person => person.name === body.name)
+  const existingPerson = persons.find((person) => person.name === body.name);
 
-  if(existingPerson) {
+  if (existingPerson) {
     return response.status(400).json({
-      error: "name must be unique"
-    })
+      error: 'name must be unique',
+    });
+  } else {
+    persons = persons.concat(person);
   }
-
-  persons = persons.concat(person)
-  
-  response.json(person)
-})
-
+  response.json(person);
+});
 
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
-
